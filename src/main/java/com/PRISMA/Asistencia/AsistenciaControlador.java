98696edb04aea0d41789d258023c8fc5e1724f4c
Klopez29 <chinolopez029@gmail.com>
2025-06-22 11:29:13 -0600

    Asistencia

--- src/main/java/com/PRISMA/Asistencia/AsistenciaControlador.java
+++ src/main/java/com/PRISMA/Asistencia/AsistenciaControlador.java
@@ -1,46 +1,61 @@
 package com.PRISMA.Asistencia;
+
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.PRISMA.Alumno.AlumnoRepositorio;
+import com.PRISMA.Entity.Alumno;
 import com.PRISMA.Entity.AsistenciaAlumno;
-import com.PRISMA.Entity.PaqueteEscolar;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
- 
 @RestController
 @RequestMapping("/AsisAlum/")
-@CrossOrigin(origins ="http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class AsistenciaControlador {
 
     @Autowired
     private AsistenciaServicio asistenciaServicio;
+
+    @Autowired
     private AsistenciaRepositorio repositorio;
-    
-    //:8080/AsisAlum/asistencia-alumno
+
+    @Autowired
+    private AlumnoRepositorio alumnoRepository;
+
+    /**
+     * Obtener todas las asistencias
+     */
     @GetMapping("/asistencia-alumno")
-    public List<AsistenciaAlumno> obtenerAsistenciaAlumnos(){
-        List<AsistenciaAlumno> asistenciaAlumnos = this.asistenciaServicio.listarAsistencia();
-
-        //  asistenciaAlumnos.forEach(asistenciaAlumno -> logger.info());
-        return asistenciaAlumnos;
+    public List<AsistenciaAlumno> obtenerAsistenciaAlumnos() {
+        return repositorio.findAll();
     }
 
-    @PostMapping("/guardar-asistencia")
-    public AsistenciaAlumno guardarAsistencia(@RequestBody AsistenciaAlumno asistenciaAlumno) {
-        
-       // return asistenciaAlumno = this.asistenciaRepositorio.save(asistenciaAlumno);
-       return repositorio.save(asistenciaAlumno);
+    /**
+     * Obtener asistencias por grado y rango de fechas
+     * Ejemplo de URL: 
+     * http://localhost:8080/AsisAlum/asistencia-alumno-filtro?idGrado=1&inicio=2025-06-01&fin=2025-06-30
+     */
+    @GetMapping("/asistencia-alumno-filtro")
+    public List<AsistenciaAlumno> obtenerAsistenciaPorGradoYFechas(
+            @RequestParam int idGrado,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin) {
+        return repositorio.findByGradoAndAnioAsistenciaAlumnos(idGrado, inicio, fin);
     }
 
+    /**
+     * Guardar nueva asistencia de un alumno
+     */
+    @PostMapping("/asistencia-alumno")
+    public AsistenciaAlumno guardarAsistenciaAlumno(@RequestBody AsistenciaAlumno asistenciaAlumno) {
+        Alumno alumnoExistente = alumnoRepository.findById(asistenciaAlumno.getAlumno().getIdAlumno())
+                .orElseThrow(() -> new RuntimeException("Alumno no encontrado"));
+
+        asistenciaAlumno.setAlumno(alumnoExistente);
+        return repositorio.save(asistenciaAlumno);
+    }
 }
--- src/main/java/com/PRISMA/Asistencia/AsistenciaRepositorio.java
+++ src/main/java/com/PRISMA/Asistencia/AsistenciaRepositorio.java
@@ -1,6 +1,11 @@
 package com.PRISMA.Asistencia;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.PRISMA.Entity.AsistenciaAlumno;
@@ -8,5 +13,12 @@ import com.PRISMA.Entity.AsistenciaAlumno;
 
 @Repository
 public interface AsistenciaRepositorio extends JpaRepository<AsistenciaAlumno, Integer>{
+    
+  
+
+    @Query("SELECT a FROM AsistenciaAlumno a WHERE a.alumno.grado.id_grado = :idGrado AND a.fecha_asistencia BETWEEN :inicio AND :fin")
+    List<AsistenciaAlumno> findByGradoAndAnioAsistenciaAlumnos(@Param("idGrado") int idGrado, @Param("inicio") LocalDate inicio, @Param("fin") LocalDate fin);
+
+    
 
 }
