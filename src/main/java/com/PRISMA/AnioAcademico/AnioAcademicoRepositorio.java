a3e07de48524fe0846b9f92052313efaffc429a5
Elias11 <mg22098@ues.edu.sv>
2025-10-28 19:06:41 -0600

    Gestion-de-años

--- src/main/java/com/PRISMA/AnioAcademico/AnioAcademicoRepositorio.java
+++ src/main/java/com/PRISMA/AnioAcademico/AnioAcademicoRepositorio.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.PRISMA.Entity.AnioAcademico;
 
 public interface AnioAcademicoRepositorio extends JpaRepository<AnioAcademico, Integer> {
-
+    boolean existsByAnio(int anio);
 }
