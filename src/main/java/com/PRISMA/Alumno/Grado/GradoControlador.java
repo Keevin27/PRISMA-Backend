9d04f87921c03d2073781133ffb3d161d4cd63fd
KevinCM07 <kcuellarmejia@gmail.com>
2025-11-14 13:26:55 -0600

    comi t

--- src/main/java/com/PRISMA/Alumno/Grado/GradoControlador.java
+++ src/main/java/com/PRISMA/Alumno/Grado/GradoControlador.java
@@ -43,11 +43,6 @@ public class GradoControlador {
         return repositorio.findAll();
     }
 
-@GetMapping("/grados/activos")
-public List<Grado> obtenerGradosActivos() {
-    return repositorio.findGradosPorAnioActivo();
-}
-
     @GetMapping("/grados/{anio}")
     public List<Grado> obtenerGradosPorAnio(@PathVariable int anio) {
         return repositorio.findByAnioAcademico_anio(anio);
